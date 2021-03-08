e9b8c78feb33601189709a216aa61cea51655c58
Lou <69117014+newlynameds@users.noreply.github.com>
2021-03-08 04:00:53 +0100

    Add files via upload

---
+++ upload/modules/Staff/language/French/general.php
@@ -0,0 +1,40 @@
+<?php 
+
+/**
+ *	STAFF PAGE MODULE
+ *	By Xemah | https://xemah.com
+ *
+**/
+
+$language = [
+
+	'title' => 'Staff',
+	'settings' => 'Page Staff',
+
+	'permission' => 'Panel > Page Staff',
+
+	'panel' => 'Panel',
+	'dashboard' => 'Tableau de Bord',
+
+	'pageTitle' => 'Titre de la page',
+	'linkPath' => 'Lien de la page',
+	'linkLocation' => 'Position du lien',
+	'navbar' => 'Navbar',
+	'navbarMoreDropdown' => 'Navbar "Plus" Dropdown',
+	'footer' => 'Footer',
+	'none' => 'Aucun(e)',
+	'navIcon' => 'Icon du lien',
+	'navOrder' => 'Ordre Nav.',
+
+	'submit' => 'Envoyez',
+	'cancel' => 'Annulé',
+
+	'settingsUpdated' => 'Les changements ont était modifié avec succès.',
+	
+	'success' => 'Succès',
+	'error' => 'Erreur',
+
+	'errorEdit' => 'Merci de rentrer les champs correctement.',
+	'errorToken' => 'Invalid token! Réessayez encore.',
+
+];
