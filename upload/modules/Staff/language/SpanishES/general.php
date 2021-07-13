27bc51356790c8656ab805510c06649624e11461
Marcos <45741056+Marcos03BR@users.noreply.github.com>
2021-07-13 17:38:18 +0200

    Create general.php
    SpanishES translation

---
+++ upload/modules/Staff/language/SpanishES/general.php
@@ -0,0 +1,42 @@
+<?php 
+
+/**
+ *	STAFF PAGE MODULE
+ *	By Xemah | https://xemah.com
+ *
+ *	SPANISH/SPAIN TRANSLATION
+ *	By ManiaNetwork
+**/
+
+$language = [
+
+	'title' => 'Personal administrativo',
+	'settings' => 'Staff',
+
+	'permission' => 'Panel administrativo > Configuración de la página del personal',
+
+	'panel' => 'Panel administrativo',
+	'dashboard' => 'Panel administrativo',
+
+	'pageTitle' => 'Título de la página',
+	'linkPath' => 'Ruta de enlace',
+	'linkLocation' => 'Ubicación del vínculo',
+	'navbar' => 'Barra de navegación',
+	'navbarMoreDropdown' => 'Desplegable "Más" en la barra de navegación',
+	'footer' => 'Pie de página',
+	'none' => 'Ninguna',
+	'navIcon' => 'Icono en la barra de navegación',
+	'navOrder' => 'Orden en la barra de navegación',
+
+	'submit' => 'Enviar',
+	'cancel' => 'Cancelar',
+
+	'settingsUpdated' => 'La configuración se ha actualizado correctamente.',
+	
+	'success' => 'Éxito',
+	'error' => 'Error',
+
+	'errorEdit' => 'Por favor, asegúrese de que los campos están rellenados correctamente.',
+	'errorToken' => 'Token inválido. Por favor, inténtelo de nuevo.',
+
+];
