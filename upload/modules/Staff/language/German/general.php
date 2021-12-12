8a0965db89dc207a1dc596947fd437fa5c1cc26b
Enrico <41052284+enno123@users.noreply.github.com>
2021-12-12 13:10:13 +0100

    German translation added
    German translation added

---
+++ upload/modules/Staff/language/German/general.php
@@ -0,0 +1,40 @@
+<?php 
+
+/**
+ *	STAFF PAGE MODULE
+ *	By Xemah | https://xemah.com translated by enno123
+ *
+**/
+
+$language = [
+
+	'title' => 'Team',
+	'settings' => 'Einstellungen der Team Seite',
+
+	'permission' => 'Panel > Einstellungen der Team Seite',
+
+	'panel' => 'Panel',
+	'dashboard' => 'Dashboard',
+
+	'pageTitle' => 'Seiten Titel',
+	'linkPath' => 'Link Pfad',
+	'linkLocation' => 'Link Location',
+	'navbar' => 'Navbar',
+	'navbarMoreDropdown' => 'Navbar "More" Dropdown',
+	'footer' => 'Footer',
+	'none' => 'Keine',
+	'navIcon' => 'Navigation Icon',
+	'navOrder' => 'Navigation reihenfolge',
+
+	'submit' => 'Senden',
+	'cancel' => 'Abbrechen',
+
+	'settingsUpdated' => 'Die Einstellungen wurden erfolgreich aktualisiert.',
+	
+	'success' => 'Erfolgreich',
+	'error' => 'Error',
+
+	'errorEdit' => 'Bitte achte darauf, das die Felder korrekt ausgefüllt sind',
+	'errorToken' => 'Ungültiger Token! Bitte versuch es erneut.',
+
+];
