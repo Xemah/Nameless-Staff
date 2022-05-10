209a503b47488584c7585c6a5552559232db80c8
Justman10000 <justmegaliga10000@gmail.com>
2022-05-10 21:38:56 +0200

    German added

---
+++ upload/modules/Staff/language/German/general.php
@@ -0,0 +1,42 @@
+<?php 
+
+/**
+ *	STAFF PAGE MODULE
+ *	By Xemah | https://xemah.com
+ *
+ *	GERMAN TRANSLATION
+ *	By Justman10000
+**/
+
+$language = [
+
+	'title' => 'Team',
+	'settings' => 'Team-Seiteneinstellungen',
+
+	'permission' => 'Panel > Team-Seiteneinstellungen',
+
+	'panel' => 'Panel',
+	'dashboard' => 'Dashboard',
+
+	'pageTitle' => 'Seitentitle',
+	'linkPath' => 'Linkpfad',
+	'linkLocation' => 'Linkort',
+	'navbar' => 'Navigationsleiste',
+	'navbarMoreDropdown' => 'Navigationsleiste "Mehr" Dropdown',
+	'footer' => 'Fußzeile',
+	'none' => 'Nichts',
+	'navIcon' => 'Navigationsicon',
+	'navOrder' => 'Navigationsanordnung',
+
+	'submit' => 'Absenden',
+	'cancel' => 'Abbrechen',
+
+	'settingsUpdated' => 'Eisntellungen wurden erfolgreich aktualisiert.',
+	
+	'success' => 'Erfolgreich',
+	'error' => 'Fehler',
+
+	'errorEdit' => 'Bitte stelle sicher, dass die Felder korrekt gefüllt sind.',
+	'errorToken' => 'Ungültiger Token! Bitte erneut versuchen.',
+
+];
